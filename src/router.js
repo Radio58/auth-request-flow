32c3bd873761d22cd0b65053fedee1f09a0529f7
radio58 <rt58gm@gmail.com>
2024-01-23 23:35:49 +0000

    login added

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET;
 
 const router = express.Router();
 
@@ -14,7 +15,14 @@ const mockUser = {
 };
 
 router.post('/login', (req, res) => {
+    const { username, password } = req.body;
 
+    if (username === mockUser.username && password === mockUser.password) {
+        const token = jwt.sign({ username: mockUser.username }, secret)
+        res.json({ token })
+    } else {
+        res.status(401).json({error: "invalid credentials"})
+    }
 });
 
 router.get('/profile', (req, res) => {
